b33a706e294816fdd08782f12a0fe22dd404dfc9
Anas Anji Abrori <anasanji27@gmail.com>
2025-10-15 09:58:20 +0700

    edit_aset.php dan delete_aset.php berhasil berfugnsi

---
+++ modules/aset/delete_aset.php
@@ -0,0 +1,16 @@
+<?php
+include '../../includes/koneksi.php';
+
+$id = $_GET['id'] ?? null;
+if (!$id) {
+  echo "<script>alert('ID tidak ditemukan!');window.location='output_aset.php';</script>";
+  exit;
+}
+
+$sql = "DELETE FROM aset_barang WHERE id = '$id'";
+if (mysqli_query($conn, $sql)) {
+  echo "<script>alert('Data aset berhasil dihapus!');window.location='output_aset.php';</script>";
+} else {
+  echo "<script>alert('Gagal menghapus data: " . mysqli_error($conn) . "');window.location='output_aset.php';</script>";
+}
+?>
